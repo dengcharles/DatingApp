423acf7d9c2f2bf9162d4b9ff018ef2da56c4dcc
Charles Deng <dengcharles@hotmail.com>
2022-05-15 21:05:43 +1000

    TestCommit

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -30,6 +30,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
+            //Test change
             return await _context.Users.FindAsync(id);
         }
     }
